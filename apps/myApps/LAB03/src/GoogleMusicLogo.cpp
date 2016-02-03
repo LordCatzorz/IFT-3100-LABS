cd056a23e89169c7c891261c9a4359e9541dc349
LordCatzorz <LordCatzorz@users.noreply.github.com>
2016-02-03 13:37:39 -0500

    Version possible du logo

--- apps/myApps/LAB03/src/GoogleMusicLogo.cpp
+++ apps/myApps/LAB03/src/GoogleMusicLogo.cpp
@@ -1,5 +1,5 @@
 #include "GoogleMusicLogo.h"
-
+#include <math.h>
 
 GoogleMusicLogo::GoogleMusicLogo()
 {
@@ -30,8 +30,81 @@ void GoogleMusicLogo::draw()
 
 	ofSetColor(orange);
 	ofDrawTriangle(-222, 20, -194, 20, -194, 144);
-	ofDrawTriangle(-166, 144, - 194, 20, -194, 144);
+	ofDrawTriangle(-166, 144, -194, 20, -194, 144);
 
 	ofDrawTriangle(222, 20, 194, 20, 194, 144);
-	ofDrawTriangle(166, 144,194, 20, 194, 144);
+	ofDrawTriangle(166, 144, 194, 20, 194, 144);
+
+	//ofSetColor(0, 128, 0);
+
+	circle topRight = circle(-113, 51, 25);
+	circle bottomRight = circle(-91, 194, 26);
+	circle bottomLeft = circle(-145, 209, 12);
+	circle topLeft = circle(-173.5, 61, 10.5);
+
+
+	ofSetColor(beige);
+	drawCircle(topRight); // top-right
+	drawCircle(bottomRight); //bottom-right
+
+	ofDrawTriangle(getPointAtAngle(topRight, 110), getPointAtAngle(bottomRight, 260), getPointAtAngle(bottomLeft, 270));
+
+	ofDrawTriangle(getPointAtAngle(topRight, 110), getPointAtAngle(bottomLeft, 270), getPointAtAngle(topLeft, 90));
+
+	ofDrawTriangle(getPointAtAngle(bottomRight, 0), getPointAtAngle(topLeft, 180), getPointAtAngle(bottomLeft, 180));
+
+	ofDrawTriangle(getPointAtAngle(topRight, 0), getPointAtAngle(bottomRight, 0), getPointAtAngle(topLeft, 180));
+
+
+	ofSetColor(orange);
+	drawCircle(bottomLeft); 
+	drawCircle(topLeft); 
+
+	ofDrawTriangle(getPointAtAngle(topLeft, 90), ofPoint(-133, 34), ofPoint(-94, 220));
+	ofDrawTriangle(getPointAtAngle(topLeft, 90), getPointAtAngle(bottomLeft,270), ofPoint(-94, 220));
+	ofDrawTriangle(getPointAtAngle(topLeft, 180), getPointAtAngle(bottomLeft, 180), getPointAtAngle(bottomLeft, 0));
+	ofDrawTriangle(getPointAtAngle(topLeft, 180), getPointAtAngle(bottomLeft, 180), getPointAtAngle(topLeft, 0));
+
+
+	//--------------------------------------------
+
+	topRight.x = -topRight.x;
+	topLeft.x = -topLeft.x;
+	bottomLeft.x = -bottomLeft.x;
+	bottomRight.x = -bottomRight.x;
+
+	ofSetColor(beige);
+	drawCircle(topRight); 
+	drawCircle(bottomRight); 
+
+	ofDrawTriangle(getPointAtAngle(topRight, 70), getPointAtAngle(bottomRight, 280), getPointAtAngle(bottomLeft, 270));
+
+	ofDrawTriangle(getPointAtAngle(topRight, 70), getPointAtAngle(bottomLeft, 270), getPointAtAngle(topLeft, 90));
+
+	ofDrawTriangle(getPointAtAngle(bottomRight, 180), getPointAtAngle(topLeft, 0), getPointAtAngle(bottomLeft, 0));
+
+	ofDrawTriangle(getPointAtAngle(topRight, 180), getPointAtAngle(bottomRight, 180), getPointAtAngle(topLeft, 0));
+
+
+	ofSetColor(orange);
+	drawCircle(bottomLeft);
+	drawCircle(topLeft);
+
+	ofDrawTriangle(getPointAtAngle(topLeft, 90), ofPoint(133, 34), ofPoint(94, 220));
+	ofDrawTriangle(getPointAtAngle(topLeft, 90), getPointAtAngle(bottomLeft, 270), ofPoint(94, 220));
+	ofDrawTriangle(getPointAtAngle(topLeft, 0), getPointAtAngle(bottomLeft, 0), getPointAtAngle(bottomLeft, 180));
+	ofDrawTriangle(getPointAtAngle(topLeft, 0), getPointAtAngle(bottomLeft, 0), getPointAtAngle(topLeft, 180));
+
 }
+
+ofPoint GoogleMusicLogo::getPointAtAngle(circle c, double angle)
+{
+	return ofPoint(c.x + c.radius * cos(angle * PI / 180), c.y - c.radius * sin(angle * PI / 180));
+}
+
+
+void GoogleMusicLogo::drawCircle(circle c)
+{
+	ofDrawCircle(c.x, c.y, c.radius);
+}
+
--- apps/myApps/LAB03/src/GoogleMusicLogo.h
+++ apps/myApps/LAB03/src/GoogleMusicLogo.h
@@ -10,5 +10,18 @@ public:
 
 	void draw();
 private:
-
+	struct circle
+	{
+		double x;
+		double y;
+		double radius;
+		circle(double _x, double _y, double _radius)
+		{
+			x = _x;
+			y = _y;
+			radius = _radius;
+		}
+	};
+	ofPoint getPointAtAngle(circle c, double angle);
+	void drawCircle(circle c);
 };
